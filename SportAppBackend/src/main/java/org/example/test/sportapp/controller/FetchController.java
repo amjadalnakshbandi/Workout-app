d4e9c36f14ca24fc058fe7fb6dee0ca958857d66
Amjad Alnakshbandi <113834304+amjadalnakshbandi@users.noreply.github.com>
2024-05-11 22:41:07 +0200

    adjust paths

--- SportAppBackend/src/main/java/org/example/test/sportapp/controller/FetchController.java
+++ SportAppBackend/src/main/java/org/example/test/sportapp/controller/FetchController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FetchController {
     @Autowired
     private JsonFileReaderService jsonFileReaderService;
-        private final String filePath = "C:\\Users\\ALNAKA\\OneDrive - NTT DATA EMEAL\\Dokumente\\Angular Schulung\\SportApp\\SportAppBackend\\src\\main\\resources\\static\\data\\data.json";
+        private final String filePath = "SportAppBackend\\src\\main\\resources\\static\\data\\data.json";
 
     @GetMapping("/")
     public Object index() {
